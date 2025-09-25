5e718d22543748422255917fb92805503b701e93
Ryth00 <josevan47106@gmail.com>
2025-09-25 16:02:35 +0700

    add form+ Suppliers column

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 //import model product
 use App\Models\Product;
+use App\Models\Category_product;
+use App\Models\Supplier;
+use Illuminate\Http\RedirectResponse;
 
+use illuminate\Support\Facades\Storage;
 //import return type view
 use Illuminate\View\View;
 
 //import request
 use Illuminate\Http\Request;
+
 
 class ProductController extends Controller
 {
@@ -21,10 +26,58 @@ class ProductController extends Controller
     public function index(): View
     {
         //get all products
-        $product= new Product;
+        $product = new Product;
         $products = $product->get_product()->latest()->paginate(10);
 
         //render view with products
         return view('products.index', compact('products'));
+    }
+
+    /**
+     * create
+     * 
+     * @return void
+     */
+    public function create(): View
+    {
+        $product = new Category_product();
+        $data['categories'] = $product->get_category_product()->get();
+
+        $supplier = new Supplier();
+        $data['supplier'] = $supplier->get_supplier()->get();
+        return view('products.create', compact('data'));
+    }
+
+    /**
+     * store
+     * 
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //var_dump($request);exit;
+        //validate form
+        $validatedData = $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required|min:5',
+            'product_category' => 'required|integer',
+            'description' => 'required|min:10',
+            'harga' => 'required|numeric',
+            'stock' => 'required|numeric'
+        ]);
+        
+        //menghandle upload file gambar
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $store_image=$image->store('images', 'public');// simpan gambar ke folder penyimpanan
+
+            $product= new Product;
+            $insert_product = $product->storeProduct($request, $image);
+
+            return redirect()->route('products.index')->with('success', 'Data Berhasil Disimpan!.');
+        }
+        
+        return redirect()->route('products.index')->with('error', 'Failed to upload image.');
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * fillable
+     * 
+     * @var array
+     */
+    protected $fillable = [
+     'image',
+     'title',
+     'product_category_id',
+     'supplier_id',
+     'description',
+     'price',
+     'stock'
+    ];
+    
 public function get_product()
 {
     $sql = $this->select(
@@ -13,10 +27,23 @@ public function get_product()
             "category_product.product_category_name as product_category_name",
             "suppliers.supplier_name as supplier_name"
         )
-        ->join('category_product', 'category_product.id', '=', 'products.product_category_id')
-        ->join('suppliers', 'suppliers.id', '=', 'products.supplier_id');
+        ->leftjoin('category_product', 'category_product.id', '=', 'products.product_category_id')
+        ->leftjoin('suppliers', 'suppliers.id', '=', 'products.supplier_id');
 
     return $sql;
+}
+
+public static function storeProduct($request, $image)
+{
+        return self::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'supplier_id' => $request->supplier_id,
+            'product_category_id' => $request->product_category,
+            'description' => $request->description,
+            'price' => $request->harga,
+            'stock' => $request->stock
+        ]);
 }
 
 }
